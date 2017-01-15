426329b1c3960b622884a17edc78b243bb7ada20
Jordan Blackmon <disasterisk@users.noreply.github.com>
2017-01-14 21:03:27 -0800

    Only accepts numbers

--- temperature/temperature.php
+++ temperature/temperature.php
@@ -2,32 +2,36 @@
 //temperature.php
 
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
-
 // display results if user has entered a temperature
 if(isset($_POST['Temperature']))
 {
     $T = $_POST['Temperature'];
-    // convert user input to a float value
-    $temperature = floatval($T);
+    if (!is_numeric($T)) {
+        echo "Please enter a valid number.";
+    }
+    else {
+        // convert user input to a float value
+        $temperature = floatval($T);
 
-    // determine which temperature type the user has selected and echo appropriate results
-    switch($_POST['Units'])
-    {
-        case 'Fahrenheit':
-            echo 'You entered ' . $T . ' degrees Fahrenheit <br />';
-            echo 'The Celsius equivalent is ' . number_format((($temperature - 32) * 5/9), 2, '.', '') . '<br />';
-            echo 'The Kelvin equivalent is ' . number_format((($temperature + 459.67) * 5/9), 2, '.', '') . '<br />';
-            break;
-        case 'Celsius':
-            echo 'You entered ' . $T . ' degrees Celsius <br />';
-            echo 'The Fahrenheit equivalent is ' . number_format((($temperature * 9/5 + 32)), 2, '.', '') . '<br />';
-            echo 'The Kelvin equivalent is ' . number_format((($temperature + 273.15)), 2, '.', '') . '<br />';
-            break;
-        case 'Kelvin':
-            echo 'You entered ' . $T . ' kelvins <br />';
-            echo 'The Fahrenheit equivalent is ' . number_format((($temperature * 9/5 - 459.67)), 2, '.', '') . '<br />';
-            echo 'The Celsius equivalent is ' . number_format((($temperature - 273.15)), 2, '.', '') . '<br />';
-            break;
+        // determine which temperature type the user has selected and echo appropriate results
+        switch($_POST['Units'])
+        {
+            case 'Fahrenheit':
+                echo 'You entered ' . $T . ' degrees Fahrenheit <br />';
+                echo 'The Celsius equivalent is ' . number_format((($temperature - 32) * 5/9), 2, '.', '') . '<br />';
+                echo 'The Kelvin equivalent is ' . number_format((($temperature + 459.67) * 5/9), 2, '.', '') . '<br />';
+                break;
+            case 'Celsius':
+                echo 'You entered ' . $T . ' degrees Celsius <br />';
+                echo 'The Fahrenheit equivalent is ' . number_format((($temperature * 9/5 + 32)), 2, '.', '') . '<br />';
+                echo 'The Kelvin equivalent is ' . number_format((($temperature + 273.15)), 2, '.', '') . '<br />';
+                break;
+            case 'Kelvin':
+                echo 'You entered ' . $T . ' kelvins <br />';
+                echo 'The Fahrenheit equivalent is ' . number_format((($temperature * 9/5 - 459.67)), 2, '.', '') . '<br />';
+                echo 'The Celsius equivalent is ' . number_format((($temperature - 273.15)), 2, '.', '') . '<br />';
+                break;
+        }
     }
 }
 // display temperature form
