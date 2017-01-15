8cc6e1412de9a917133360aa8af675af1aef2d71
Jordan Blackmon <disasterisk@users.noreply.github.com>
2017-01-14 20:48:20 -0800

    Simplifying variables
    Changed "TemperatureType" to Units and enclosed $_POST['Temperature'] in a variable.

--- temperature/temperature.php
+++ temperature/temperature.php
@@ -6,46 +6,43 @@ define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 // display results if user has entered a temperature
 if(isset($_POST['Temperature']))
 {
-    
+    $T = $_POST['Temperature']
     // convert user input to a float value
-    $temperature = floatval($_POST['Temperature']);
-    
+    $temperature = floatval($T);
+
     // determine which temperature type the user has selected and echo appropriate results
-    switch($_POST['TemperatureType'])
+    switch($_POST['Units'])
     {
         case 'Fahrenheit':
-            echo 'You entered ' . $_POST['Temperature'] . ' degrees Fahrenheit <br />';
+            echo 'You entered ' . $T . ' degrees Fahrenheit <br />';
             echo 'The Celsius equivalent is ' . number_format((($temperature - 32) * 5/9), 2, '.', '') . '<br />';
             echo 'The Kelvin equivalent is ' . number_format((($temperature + 459.67) * 5/9), 2, '.', '') . '<br />';
             break;
         case 'Celsius':
-            echo 'You entered ' . $_POST['Temperature'] . ' degrees Celsius <br />';
+            echo 'You entered ' . $T . ' degrees Celsius <br />';
             echo 'The Fahrenheit equivalent is ' . number_format((($temperature * 9/5 + 32)), 2, '.', '') . '<br />';
             echo 'The Kelvin equivalent is ' . number_format((($temperature + 273.15)), 2, '.', '') . '<br />';
             break;
         case 'Kelvin':
-            echo 'You entered ' . $_POST['Temperature'] . ' kelvins <br />';
+            echo 'You entered ' . $T . ' kelvins <br />';
             echo 'The Fahrenheit equivalent is ' . number_format((($temperature * 9/5 - 459.67)), 2, '.', '') . '<br />';
             echo 'The Celsius equivalent is ' . number_format((($temperature - 273.15)), 2, '.', '') . '<br />';
             break;
     }
-    
-
-    
 }
 // display temperature form
 else
 {
-    echo 
+    echo
     '
     <form method="post" action="' . THIS_PAGE . '">
-    Temperature <input type="text" name="Temperature" /> <br />  
-    <select name="TemperatureType">
+    Temperature <input type="text" name="Temperature" /> <br />
+    <select name="Units">
         <option value="Fahrenheit">Fahrenheit</option>
         <option value="Celsius">Celsius</option>
         <option value="Kelvin">Kelvin</option>
     </select> <br /> <br />
     <input type="submit" value="Convert" />
-    </form>    
+    </form>
     ';
 }
